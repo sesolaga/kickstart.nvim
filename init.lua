a05d5ac09fadfa06bd8b544430e4cd84cddb9ede
Sergei Solagaian <solagaian.sergei@yandex.ru>
2025-12-25 16:26:43 +0300

    fixes

--- init.lua
+++ init.lua
@@ -1038,10 +1038,23 @@ require('lazy').setup({
       {
         'L3MON4D3/LuaSnip',
         version = '2.*',
-        build = (function()
+	build = (function()
           -- Build Step is needed for regex support in snippets.
           -- This step is not supported in many windows environments.
-	@@ -797,95 +798,74 @@ require('lazy').setup({
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          -- `friendly-snippets` contains a variety of premade snippets.
+          --    See the README about individual language/framework/plugin snippets:
+          --    https://github.com/rafamadriz/friendly-snippets
+          -- {
+          --   'rafamadriz/friendly-snippets',
+          --   config = function()
+          --     require('luasnip.loaders.from_vscode').lazy_load()
           --   end,
           -- },
         },
