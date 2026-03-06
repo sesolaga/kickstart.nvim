baf0b01e4808f6cf384897c3c918b559f313ab28
Sergei Solagaian <solagaian.sergei@yandex.ru>
2026-03-06 13:14:07 +0300

    Save buffers on session switch

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -2,7 +2,19 @@ return {
   'rmagatti/auto-session',
   lazy = false,
   opts = {
-    suppressed_dirs = { '~/', '~/Downloads', '/tmp' },
+    suppressed_dirs = { '~/Downloads', '/tmp' },
+    auto_save = true,
+    pre_save_cmds = { 'silent! wa' },
+    -- Show session picker when opening nvim in home dir with no args
+    no_restore_cmds = {
+      function()
+        if vim.fn.getcwd() == vim.fn.expand '~' and vim.fn.argc() == 0 then
+          vim.schedule(function()
+            vim.cmd 'AutoSession search'
+          end)
+        end
+      end,
+    },
   },
   keys = {
     { '<leader>sp', '<cmd>AutoSession search<cr>', desc = '[S]earch [P]rojects (sessions)' },
